beac345ae16635c486f2a71f474d240b6c04ad79
danlevik <leviol80@gmail.com>
2022-05-02 12:38:52 +0300

    Стили работают, если пользователь не зарегистрирован

--- src/main/java/com/project/elib/config/WebSecurityConfig.java
+++ src/main/java/com/project/elib/config/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -22,12 +23,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
 
                 .authorizeRequests()
-                    .antMatchers("/registration", "/resources/**", "/static/**", "static/style.css", "/css/**", "/js/**", "/img/**", "/icon/**").permitAll()
+                    .antMatchers("/login", "/registration", "/style.css", "/css/**", "/js/**", "/img/**", "/icon/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
